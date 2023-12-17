5bf99ada8c4a19e488b9bdcbc71783d1358873ea
demeralde <9282837+demeralde@users.noreply.github.com>
2023-12-17 14:24:56 +1030

    Update tailwind.config.js colors
    - Replace `indigo` with `primary` alias
    - Remove all other colors besides `gray`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import { type Config } from "tailwindcss";
 import { fontFamily } from "tailwindcss/defaultTheme";
+import colors from "tailwindcss/colors";
 
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
+    colors: {
+      primary: colors.indigo,
+      gray: colors.gray,
+    },
     extend: {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
