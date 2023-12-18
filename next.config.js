4710a26775fda74399b9c61ff3ef7cb5c7d24a7e
demeralde <9282837+demeralde@users.noreply.github.com>
2023-12-18 23:54:16 +1030

    Add redirect for index page

--- next.config.js
+++ next.config.js
@@ -7,6 +7,15 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/posts",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default config;
