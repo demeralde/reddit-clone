d7d634880569d7817f8bea5de36e2f82e4881318
demeralde <9282837+demeralde@users.noreply.github.com>
2023-12-18 23:55:33 +1030

    Update global types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,10 @@
+import { type AvatarProps } from "~/app/_components/Avatar";
+
 type ID = string;
 
 type VoteType = "upvote" | "downvote";
+
+type Author = {
+  username: AvatarProps["name"];
+  avatarSrc: AvatarProps["src"];
+};
